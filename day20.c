b77b9902f51262b4f13812e60b0a609e52811849
Grégoire de Massé <gregoire@cardiologs.com>
2022-12-20 20:20:10 +0100

    day 20

--- day20.c
+++ day20.c
@@ -6,47 +6,47 @@
 typedef struct node {
     struct node* prev;
     struct node* next;
-    int val;
+    long val;
 } node;
 
 void debug_ll(node** nodes, size_t nn) {
     for (size_t i=0;i < nn;i++) {
-        fprintf(stderr, "(%d) - %d - (%d)\n", nodes[i]->prev->val, nodes[i]->val, nodes[i]->next->val);
+        fprintf(stderr, "(%ld) - %ld - (%ld)\n", nodes[i]->prev->val, nodes[i]->val, nodes[i]->next->val);
     }
 }
 
-void l_move(node* n) {
-    node* new_pos = n;
+void l_move(node* n, size_t nn) {
+
+    if (n->val == 0) {
+        return;
+    } 
+
+    node* new_prev = n, *new_next = n;
+    // either do steps = val % node_count - 1
+    // or do not modulo and first remove the current element from the list to skip itself
     if (n->val > 0) {
-        for (size_t i=0;i<n->val;i++) {
-            new_pos = new_pos->next;
+        for (size_t i=0;i<(n->val % nn);i++) {
+            new_prev = new_prev->next;
         }
+        new_next = new_prev->next;
     } else {
-        for (size_t i=0;i<=-n->val;i++) {
-            new_pos = new_pos->prev;
+        // one off when moving backward
+        for (size_t i=0;i<(-n->val % nn);i++) {
+            new_next = new_next->prev;
         }
+        new_prev = new_next->prev;
     }
 
-    if (new_pos == n) {
-        return;
-    }
-    // fprintf(stderr, "Setting %d in place of %d\n", n->val, new_pos->val);
+
     // remove current node
-    // fprintf(stderr, "Setting prev (%d -> %d) to n next %d\n", n->prev->val, n->prev->next->val, n->next->val);
     n->prev->next = n->next;
-    // fprintf(stderr, "Setting next (%d -> %d) to n prev %d\n", n->next->val, n->next->prev->val, n->prev->val);
     n->next->prev = n->prev;
 
-    // set n after new_pos
-    // fprintf(stderr, "Setting n prev (%d) to n %d\n", n->prev->val, new_pos->val);
-    n->prev = new_pos;
-    // fprintf(stderr, "Setting n next (%d) to n %d\n", n->next->val, new_pos->next->val);
-    n->next = new_pos->next;
+    n->prev = new_prev;
+    n->next = new_next;
 
-    // set new_pos before n
-    // fprintf(stderr, "Setting new pos (%d) next (%d) next to n %d\n", new_pos->val, new_pos->next->val, n->val);
-    new_pos->next->prev = n;
-    new_pos->next = n;
+    new_prev->next = n;
+    new_next->prev = n;
 }
 
 int main(int argc, char* argv[]) {
@@ -56,15 +56,16 @@ int main(int argc, char* argv[]) {
         return 1;
     }
 
-    int val;
+    long val;
 
     node* nodes[N];
-    node* lstart;
+    node* lstart = NULL;
     size_t node_count = 0;
 
-    while (fscanf(file, "%d\n", &val) == 1)
+    while (fscanf(file, "%ld ", &val) == 1)
     {
         node* new_node = malloc(sizeof(node));
+        val = val * 811589153;
         if (val == 0) {
             lstart = new_node;
         }
@@ -78,45 +79,47 @@ int main(int argc, char* argv[]) {
     nodes[0]->prev = nodes[node_count - 1];
     nodes[node_count - 1]->next = nodes[0];
 
-    node* n = nodes[0];
-    do {
-        fprintf(stderr, "%d ", n->val);
-        n = n->next;
-    }
-    while (n != nodes[0]);
-    fprintf(stderr, "\n");
+    // node* n = nodes[0];
+    // do {
+    //     fprintf(stderr, "%d ", n->val);
+    //     n = n->next;
+    // }
+    // while (n != nodes[0]);
+    // fprintf(stderr, "\n");
      
+    for (size_t k=0;k < 10;k++) {
     for (size_t i=0;i<node_count;i++) {
         // check linked list state
         // debug_ll(nodes, node_count);
-        l_move(nodes[i]);
+        l_move(nodes[i], node_count - 1); // -1 very important to not count passing itself as moving one forward when steps > node_count
         // debug_ll(nodes, node_count);
         // check linked list state
         // node* n = nodes[0];
         // do {
         //     fprintf(stderr, "%d ", n->val);
         //     n = n->next;
-        // }
-        // while (n != nodes[0]);
+        // } while (n != nodes[0]);
         // fprintf(stderr, "\n");
     }
+    }
+
 
     // find elem 1000
-    int dx = 1000 % node_count;
-    int dxs[3] = {1000, 2000, 3000};
-    int sum = 0;
+    long dxs[3] = {1000, 2000, 3000};
+    long sum = 0;
     for (size_t k=0;k < 3;k++) {
-        int dx = dxs[k] % node_count;
+        long dx = dxs[k] % node_count;
         node* start = lstart->next;
         for (size_t i=1;i<dx;i++) {
             start = start->next;
         }
-        printf("%ld: %d\n", k+1, start->val);
+        printf("%ld (%ld): %ld\n", k+1, dx, start->val);
         sum += start->val;
     }
 
     // 9663 too low
-    printf("Sum %d\n", sum);
+    // part 2 1186475640 toolow
+    printf("Sum %ld\n", sum);
 
     return 0;
 }
